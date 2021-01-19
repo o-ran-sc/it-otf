31e571fb13303e406786aa6d3c7d7bc12dc694bf
Chen, Jackie (jv246a) <jv246a@att.com>
2021-01-19 14:59:43 -0500

    updating helm/docker files and database config files
    Change-Id: I0190970f39afdc0313205d88d9055001f3f095ed
    Signed-off-by: Chen, Jackie <jv246a@att.com>

--- otf-helm/otf/charts/databases/charts/mysqldb/scripts/init_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 20;
-mysql -u otfuser otf_camunda -pToday.123 < /data/scripts/mysql_engine_7.10.0.sql
-mysql -u otfuser otf_camunda -pToday.123 < /data/scripts/mysql_identity_7.10.0.sql
-
